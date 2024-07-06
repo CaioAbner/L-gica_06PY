36c5fcf4ced259ae8edeff30fff80d1d6eb0bd0b
Caio Abner de Oliveira Santos <129506960+CaioAbner@users.noreply.github.com>
2024-07-06 13:53:27 -0300

    Add files via upload

---
+++ Logica06_PY/index.py
@@ -0,0 +1,60 @@
+# ATIVIDADE 01
+
+# cont = 0
+# nota_total = 0
+# media = 0
+
+# while cont < 4:
+#     notas = float(input('Digite suas notas: '))
+#     nota_total += notas
+#     cont += 1
+
+# media = nota_total / cont
+# print(f'Sua média foi de: {media}')
+
+# ATIVIDADE 02
+
+# cont = 0
+# nota_total = 0
+# media = 0
+
+# while True:
+#     numero = float(input('Digite um número: '))
+#     if (numero > 0):
+#         nota_total += numero
+#         cont += 1
+#     elif (numero < 0):
+#         break
+
+# media = nota_total / cont
+# print(f'Sua média dos números digitados foi igual a: {media}')
+
+# ATIVIDADE 03
+
+# fatorial = 1
+
+# numero = int(input('Digite um número e veja o fatorial do mesmo: '))
+
+# if (numero < 0):
+#     print(f'Não existem fatoriais de números negativos.')
+# elif (numero == 0):
+#     print(f'O fatorial do número 0 é 1.')
+# else:
+#     while numero > 0:
+#         fatorial *= numero
+#         numero -= 1
+#     print(f'O fatorial do número digitado foi de: {fatorial}')
+
+# ATIVIDADE 04
+
+# numero = int(input('Digite um número e veja se ele é primo ou não: '))
+
+# if (numero % numero == 0 and numero / 1):
+#     print(f'O número digitado é primo!')
+# else:
+#     print(f'O número digitado não é primo!')
+
+# ATIVIDADE 05
+
+#   5. Faça um programa que receba um número “n” e mostre a
+#   sequência de F ibonacci até o enésimo elemento.
